dacb8fe2e5537b819446775392082ae3eb69c8bb
ialoig <giovannilai86@gmail.com>
2021-12-03 12:48:38 +0100

    Added check wallet

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
 import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
 import React from "react";
 
-// Constants
+
 const GITHUB_LINK = "https://github.com/ialoig";
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
-  // Render Methods
-  const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
-      Connect to Wallet
-    </button>
-  );
 
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header gradient-text">Colorful Trooper</p>
-          <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
-          </p>
-          {renderNotConnectedContainer()}
-        </div>
-        <div className="footer-container">
-          <p>Build with ❤️ by 
-          <a
-            className="footer-text"
-            href={GITHUB_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >ialoig</a></p>
-        </div>
-      </div>
-    </div>
-  );
+
+	useEffect(() => {
+		checkIfWalletIsConnected()
+	}, [])
+
+
+	// checking if wallet is connected or not
+	const checkIfWalletIsConnected = () => {
+		const {ethereum} = window
+		if (!ethereum) {
+			console.log("Wallet not connected")
+		} else {
+			console.log("Wallet already connected: ", ethereum)
+		}
+	}
+
+	// Render Methods
+	const renderNotConnectedContainer = () => (
+		<button className="cta-button connect-wallet-button">
+			Connect to Wallet
+		</button>
+	);
+
+	return (
+		<div className="App">
+			<div className="container">
+					<div className="header-container">
+					<p className="header gradient-text">Colorful Trooper</p>
+					<p className="sub-text">
+						Each unique. Each beautiful. Discover your NFT today.
+					</p>
+					{renderNotConnectedContainer()}
+				</div>
+				<div className="footer-container">
+					<p className="footer-text">Build with ❤️ by 
+					<a
+						className="footer-text"
+						href={GITHUB_LINK}
+						target="_blank"
+						rel="noreferrer"
+					>ialoig</a></p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
