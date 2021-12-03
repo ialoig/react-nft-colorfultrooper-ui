48d638a91edbca7b5ba250856ec80f7d5e395b81
ialoig <ialoig@protonmail.com>
2021-12-03 15:41:03 +0100

    Added account state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles/App.css"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 
 const GITHUB_LINK = "https://github.com/ialoig"
@@ -8,18 +8,33 @@ const TOTAL_MINT_COUNT = 50
 
 const App = () => {
 
+	const [account, setAccount] = useState()
+
 	useEffect(() => {
 		checkIfWalletIsConnected()
 	}, [])
 
 
 	// checking if wallet is connected or not
-	const checkIfWalletIsConnected = () => {
+	const checkIfWalletIsConnected = async () => {
 		const { ethereum } = window
 		if (!ethereum) {
 			console.log("Wallet not connected")
+			return
 		} else {
 			console.log("Wallet already connected: ", ethereum)
+		}
+
+		// checking authorization on user account
+		const accounts = await ethereum.request({ method: "eth_accounts" })
+		console.log("Accounts authorization found: ", accounts)
+
+		if (accounts.length !== 0) {
+			const account = accounts[0]
+			setAccount(account)
+			console.log("Account is : ", account)
+		} else {
+			console.log("No authorized account found")
 		}
 	}
 
@@ -36,7 +51,7 @@ const App = () => {
 				<div className="header-container">
 					<p className="header gradient-text">Colorful Trooper</p>
 					<p className="sub-text">
-						Each unique. Each beautiful. Discover your NFT today.
+						Each unique. Each beautiful. Each colorful. Mint your NFT today.
 					</p>
 					{renderNotConnectedContainer()}
 				</div>
